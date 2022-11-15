7e7ec347033545e650bdec6b33338c32135923a4
joffrey <joffreybena@gmail.com>
2022-11-15 17:05:33 +0100

    Joffrey.py

---
+++ Joffrey.py
@@ -0,0 +1,21 @@
+jourdesem=["Lundi","Mardi","Mercredi","Jeudi","Vendredi"]
+elem = jourdesem[4]
+print(elem)
+
+total = (5+4)*(9-3)
+print(total)
+
+a=3
+b=4
+c=5
+multipl = ((a+b)*c)
+print(multipl)
+
+a = input ("Veuillez saisir un nombre \n")
+b = input ("Veuillez saisir un deuxieme nombre \n")
+c = input ("Veuillez saisir un troisieme nombre \n")
+print("A + B multiplié par C est :" + str((int (a) +int (b) ) *int (c)))
+
+a = input ("Veuillez saisir un nombre \n")
+b = input ("Veuillez saisir un deuxieme nombre \n")
+print("La partie entière de la division de A par B est :" + str(int (a) /int (b) ))
